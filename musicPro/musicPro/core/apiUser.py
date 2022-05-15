e9a3d6bc4a26ecf5de7178663f8bcf3016eac76f
BrandonSepu <bra.sepulvedam@duocuc.cl>
2022-05-14 22:41:49 -0400

    api user crud completed 14/05/22

--- musicPro/musicPro/core/apiUser.py
+++ musicPro/musicPro/core/apiUser.py
@@ -4,87 +4,170 @@ import json
 
 
 def getAllUsers():
-    key="users"
-    url="https://springbootusers.herokuapp.com/" + key
-
-    response = requests.get(url)
-    data = response.json()
-    pprint(data)
-    return data
+    try:
+        key="users"
+        url="https://springbootusers.herokuapp.com/" + key
+        respuesta = requests.get(url)
+        if respuesta.status_code == 200:
+            data = respuesta.json()
+            pprint(data)
+            print(print("se logró"+ str(respuesta)))
+        else:
+            print(print("NO se logró" + str(respuesta)))
+        return data
+    except Exception as e:
+        print(e)
 
 #getAllUsers()
 
 def getUser(id):
-    key="user/"
-    url="https://springbootusers.herokuapp.com/" + key + str(id)
 
-    response = requests.get(url)
-    
-    data = response.json()
-    print(data)
-    return data
+    try:
+        url="https://springbootusers.herokuapp.com/user/"+ str(id)
+        respuesta = requests.get(url)
+        if respuesta.status_code == 200:
+            print(print("se logró"+ str(respuesta)))
+            data = respuesta.json()
+            print(str(respuesta))
+            print(data)
+            return data
+        else:
+            print(print("NO se logró, id no encontrada"+ str(respuesta)))
+        
+    except Exception as e:
+        print(e)
 
 #getUser(4)
 
 def getUserByEmail(email):
-    key="users"
-    url="https://springbootusers.herokuapp.com/" + key
-
-    response = requests.get(url)
-    data = response.json()
-    for i in data:
-        print(i["email_user"])
-        if i["email_user"] == email:
-            print("lo encontre")
-            data = i
-            print(data)
-            break
+    try:
+        key="users"
+        url="https://springbootusers.herokuapp.com/" + key
+        data=False
+        respuesta = requests.get(url)
+        if respuesta.status_code == 200:
+            print(print("se logró"+ str(respuesta)))
+            data = respuesta.json()
+            for i in data:
+                print(i["email_user"])
+                if i["email_user"] == email:
+                    print("lo encontre")
+                    data = i
+                    print(data)
+                    break
+                else:
+                    print("NO lo encontre")
+                    data = False
+                    pprint(data)
+                    break
         else:
-           print("NO lo encontre")
-           data = False
-           pprint(data)
-    
-    return data
+            print(print("NO se logró, id no encontrada"+ str(respuesta))) 
+        return data
+    except Exception as e:
+        print(e)
         
 #getUserByEmail("brandonsepux@gmail.com");
 
 def login(email, password):
-    key="users"
-    url="https://springbootusers.herokuapp.com/" + key
+    try:
+        key="users"
+        url="https://springbootusers.herokuapp.com/" + key
+        respuesta = requests.get(url)
+        if respuesta.status_code == 200:
+            print(print("se logró"+ str(respuesta)))
+            data = respuesta.json()
 
-    response = requests.get(url)
-    data = response.json()
-
-    for i in data:
-        #print(i["email_user"])
-        if i["email_user"] == email and i["pass_user"] == password:
-            print("lo encontre, usuario logueado")
-            data = i
-            pprint(data)
-            break
-            
+            for i in data:
+                #print(i["email_user"])
+                if i["email_user"] == email and i["pass_user"] == password:
+                    print("lo encontre, usuario logueado")
+                    data = i
+                    pprint(data)
+                    break
+                else:
+                    print("NO lo encontre, clave o email incorrectos")
+                    data = False
+                    pprint(data)
+                    break
         else:
-           print("NO lo encontre, clave o email incorrectos")
-           data = False
-           pprint(data)
-           break
-    return data 
+            print(print("NO se logró, id no encontrada"+ str(respuesta)))
+        return data 
+    except Exception as e:
+        print(e)
     
 #login("brandonsepux@gmail.com", "cesariño")
 
 def loadUser(nom_user,rut_user,age_user,tipo_user,email_user,pass_user):
-    
-    url="https://springbootusers.herokuapp.com/load"
     try:
-        dato = {"nom_user":"vanesita","rut_user":"20447641-2","age_user":"21","tipo_user":"bodeguera","email_user":"val11e@gmail.com","pass_user":"vane"}
-        response = requests.post(url, json = dato )
-        print("se logró"+ str(response)) 
+        url="https://springbootusers.herokuapp.com/load"
+        respuesta = False
+    
+        if len(rut_user) <= 10:
+            dato = {"nom_user":nom_user,"rut_user":rut_user,"age_user":age_user,"tipo_user":tipo_user,"email_user":email_user,"pass_user":pass_user}
+            respuesta = requests.post(url, json = dato )
+            if respuesta.status_code == 200:
+                print(print("se logró"+ str(respuesta)))
+            else:
+                print(print("NO se logró, id no encontrada"+ str(respuesta)))
+        else:
+            print("el largo del rut excede el maximo")
     except Exception as e:
         print("No se logró")
         print(e)
         data = False
         pprint(data)
-    return  response  
+    return  respuesta
+      
 
-#loadUser("Vanesia","243126141-2","21","bodeguera","val11e@gmail.com","vane")  
-getAllUsers()
+#loadUser("Vanesia","24126141-2","21","bodeguera","val11e@gmail.com","vane")  
+
+def updateUser(id_user,nom_user,rut_user,age_user,tipo_user,email_user,pass_user):
+    try:
+        url="https://springbootusers.herokuapp.com/update"
+        respuesta = False
+    
+        if len(rut_user) <= 10:
+            dato = {'id_user': id_user,"nom_user":nom_user,"rut_user":rut_user,"age_user":age_user,"tipo_user":tipo_user,"email_user":email_user,"pass_user":pass_user}
+            respuesta = requests.put(url, json = dato )
+            if respuesta.status_code == 200:
+                print(print("se logró"+ str(respuesta)))
+            else:
+                print(print("NO se logró, id no encontrada"+ str(respuesta)))
+        else:
+            print("el largo del rut excede el maximo")
+    except Exception as e:
+        print("No se logró, hubo un error")
+        print(e)
+        data = False
+        pprint(data)
+    return  respuesta  
+
+#getAllUsers()
+#updateUser(94,'vanesita','20447641-2',21,'bodeguera','val11e@gmail.com','vane',)
+#updateUser(94,'vanesita','20447641-2',23,'vendedora','val11e@gmail.com','vane',)
+
+def delUserById(id):
+    try:
+        data = getUser(id)
+        respuesta = False
+        url="https://springbootusers.herokuapp.com/dUser/" + str(id)
+        if id >= 0:
+            respuesta = requests.delete(url)
+            if respuesta.status_code == 200:
+                print("se logró"+ str(respuesta) + " eliminste a : " + data["nom_user"])
+                print("se logró")
+            else:
+                print(print("NO se logró, id no encontrada"+ str(respuesta)))
+        else:
+            print("id no encontrada = "+id)
+        
+        return respuesta
+    except Exception as e:
+        print("No se logró, hubo un error")
+        print(e)
+
+#delUserById(74)
+#getUser(4)
+#getAllUsers()
+    
+    
