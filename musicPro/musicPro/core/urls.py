c5349f104a7d2ce9f01fad6566d581b137ee8502
BrandonSepu <bra.sepulvedam@duocuc.cl>
2022-05-14 23:23:16 -0400

    login template

--- musicPro/musicPro/core/urls.py
+++ musicPro/musicPro/core/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path("base/", views.base, name="base"),
     path("", views.index, name="index"),
     path("productos/", views.productos, name="productos"),
+    #login
+    path("login/", views.log_in, name="login"),
+    #register
 ]
--- musicPro/musicPro/core/views.py
+++ musicPro/musicPro/core/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
-
+from core.apiUser import login
 #from .utils import render_to_pdf, sendEmail
 #from django.http import HttpResponse
 #from .forms import contactForm, paceinteForm, horaMedicaForm
@@ -18,3 +16,10 @@ def index(request):
 
 def productos(request):
     return render(request, 'web/productos.html')
+
+def log_in(request):
+    email1 = request["username"]
+    print(email1 + " " + pass1)
+    pass1 = request["password"]
+    #context = login(email1,pass1)
+    return render(request, 'web/login.html')
